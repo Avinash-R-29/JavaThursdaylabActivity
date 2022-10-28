e5136bb411420354ea71e04aea66e23ddff5335d
Avinash-R-29 <7338avinash@gmail.com>
2022-10-28 08:43:24 +0530

    Lab Activity03

---
+++ Lab_Activity/src/com/lab_activity03/SwapTwoNum.java
@@ -0,0 +1,33 @@
+package com.lab_activity03;
+
+import java.util.*;
+class Swap
+{
+	int a,b;
+	
+	void swap(Swap sw)
+	{
+		int tmp=sw.a;
+		sw.a=sw.b;
+		sw.b=tmp;
+	}
+	void display()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+	}
+}
+public class SwapTwoNum {
+
+
+	public static void main(String[] args) {
+		Swap sw=new Swap();
+		sw.display();
+		sw.swap(sw);
+		System.out.println(sw.a+" "+sw.b);
+
+	}
+
+}
