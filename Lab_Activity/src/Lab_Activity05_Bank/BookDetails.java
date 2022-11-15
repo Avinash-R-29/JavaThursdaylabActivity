07b2bb4c3cb04e63c9d074143fde5396cdcb41e0
Avinash-R-29 <7338avinash@gmail.com>
2022-11-15 08:02:45 +0530

    Lab_Activity05

---
+++ Lab_Activity/src/Lab_Activity05_Bank/BookDetails.java
@@ -0,0 +1,18 @@
+package Lab_Activity05_Bank;
+
+public class BookDetails {
+	static void display(Book b) {
+		System.out.println("Book type : " + b.getBookType());
+		System.out.println("Book Author name : " + b.getAr());
+		System.out.println("Book price : " + b.getPrice());
+		System.out.println("Year of publictaion of Book: " + b.getYearOfPublication());
+
+	}
+
+	public static void main(String[] args) {
+			
+		Book b = new Book("romeo and juliet", 1597,4899.00, new Author("William Shakespeare ,", 66));
+		display(b);
+	}
+
+}
---
+++ Lab_Activity/src/Lab_Activity05_Shapes/ShapeMain.java
@@ -0,0 +1,19 @@
+package Lab_Activity05_Shapes;
+
+public class ShapeMain {
+	public String shape;
+	ShapeMain(String n)
+	{
+		this.shape=n;
+	}
+
+	public String getShape() {
+		return shape;
+	}
+
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+	
+	
+}
